4f87ff7267033c1171aea425ca033e2b8c488983
Harut-2025 <ashryan@inbox.ru>
2025-05-09 21:43:26 +0400

    1

--- src/Components/Ul/Input/Input.jsx
+++ src/Components/Ul/Input/Input.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
 import styles from './Input.module.scss'
-
 
 export default function Input() {
   return (
     <div className={styles.inputDiv}>
       <input type="text" className={styles.input} />
-      <img src="./Assets/Img/Group.png" alt="" className={styles.inputButton} />
+      <img src="./Assets/Img/Group.png" alt="" className={styles.inputButton}   />
     </div>
   )
 }

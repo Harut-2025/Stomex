a07e5284d555ab077837a1e719a8bc73126abcf8
Harut-2025 <ashryan@inbox.ru>
2025-05-10 22:14:01 +0400

    1

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -6,7 +6,13 @@ import styles from './Order.module.scss'
 export default function Order() {
     const { t, i18n } = useTranslation();
     return (
+
         <div className={styles.maping}>
+            <div className={styles.mapBlock}>
+                <iframe src="https://yandex.ru/map-widget/v1/?ll=44.512546%2C40.177628&z=10">
+
+                </iframe>
+            </div>
             <div className={styles.matherOfOrder}>
                 <div className={styles.order}><p>{t('orderAll')}</p></div>
                 <div className={styles.order}><p>{t('orderMassa')}</p></div>
